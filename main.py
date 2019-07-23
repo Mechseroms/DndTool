33be1091fce1f3d6977eda663e54f616c07bd622
Jadowyne Ulve <mechserom@gmail.com>
2019-07-23 00:28:20 -0500

    First Commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import csv
 
-print("Hello World!")
+
+def load_sheet(path_to_csv):
+    try:
+        with open(path_to_csv, 'r') as file:
+            sheet = [row for row in csv.reader(file)]
+    except FileNotFoundError:
+        print(f"{path_to_csv} does not exist")
+        sheet = []
+
+    return sheet
+
+
+print(load_sheet('sheets/players.csv'))
